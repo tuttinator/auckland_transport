61dc82cd863a17750d854539a8b072a75ba0d6b9
Caleb Tutty <caleb@prettymint.co.nz>
2014-05-27 23:37:34 +1200

    Commenting API endpoints

---
+++ lib/auckland_transport/gtfs.rb
@@ -0,0 +1,58 @@
+module AucklandTransport
+  class GTFS
+    #
+    # GET Agencies list
+    #     /gtfs/agency
+    # GET Calendar list
+    #     /gtfs/calendar
+    # GET Calendar by service id
+    #     /gtfs/calendar/serviceId/:service_id
+    # GET Calendar exception list
+    #     /gtfs/calendarDate
+    # GET Calendar exception by service id
+    #     /gtfs/calendarDate/serviceId/:service_id
+    # GET Routes list
+    #     /gtfs/routes
+    # GET Routes list filtered by id
+    #     /gtfs/routes/routeId/:route_id
+    # GET Routes list filtered by long name
+    #     /gtfs/routes/routeLongName/:route_long_name
+    # GET Routes list filtered by short name
+    #     /gtfs/routes/routeShortName/:route_short_name
+    # GET Routes search
+    #     /gtfs/routes/search/:search_text
+    # GET Routes, search radius around a position (latitude and longitude)
+    #     /gtfs/routes/geosearch
+    # GET Routes search by stop id
+    #     /gtfs/routes/stopid/:stop_id
+    # GET Shape by Id
+    #     /gtfs/shapes/shapeId/:shape_id
+    # GET Shape by trip id
+    #     /gtfs/shapes/tripId/:trip_id
+    # GET Shape geometry by Id
+    #     /gtfs/shapes/geometry/:shape_id
+    # GET Stops list
+    #     /gtfs/stops
+    # GET Stop by Id
+    #     /gtfs/stops/stopId/:stop_id
+    # GET Stop by code
+    #     /gtfs/stops/stopCode/:stop_code
+    # GET Stops search by name
+    #     /gtfs/stops/search/:search_text
+    # GET Stops search by postion
+    #     /gtfs/stops/geosearch
+    # GET Stops times by stop id
+    #     /gtfs/stopTimes/stopId/:stop_id
+    # GET Stops times by trip id
+    #     /gtfs/stopTimes/tripId/:trip_id
+    # GET Trips list
+    #     /gtfs/trips
+    # GET Trips by trip id
+    #     /gtfs/trips/tripId/:trip_id
+    # GET Trips by route id
+    #     /gtfs/trips/routeid/:route_id
+    #
+    #
+    #
+  end
+end
---
+++ lib/auckland_transport/real_time.rb
@@ -0,0 +1,23 @@
+module AucklandTransport
+  class RealTime
+    #
+    # Endpoint: /public/realtime/vehiclelocations
+    #
+    # Entities have:
+    #
+    # an id
+    #
+    # vehicle
+    # - trip
+    #   - trip id
+    #   - trip
+    #   - schedule_relationships # currently always zero
+    # - vehicle
+    #   - id
+    # - position
+    #   - lat
+    #   - long
+    # - timestamp
+    #
+  end
+end
