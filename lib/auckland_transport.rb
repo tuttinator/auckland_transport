08924ce3849a3edc4461ece2172d82ae70db9e52
Caleb Tutty <caleb@prettymint.co.nz>
2014-06-16 23:06:05 +1200

    Added ghost object macro

--- lib/auckland_transport.rb
+++ lib/auckland_transport.rb
@@ -1,8 +1,9 @@
 require 'httparty'
+lib = File.expand_path('../../lib', __FILE__)
+$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
 require 'auckland_transport/version'
+require 'auckland_transport/lazy_load'
 require 'auckland_transport/api_client'
 require 'auckland_transport/real_time'
 require 'auckland_transport/gtfs'
 
-module AucklandTransport
-end
